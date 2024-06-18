e9a701ee6147fe9aa16db15167c794279eaf2eb4
Sofie <sofiebrink@yahoo.com>
2024-06-18 20:18:28 +0200

    Voltooid Project

--- Examen-Opdracht/App/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Examen-Opdracht/App/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,19 +2,59 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText serialNumber;
+    private EditText password;
+    private Button loginButton;
+
+    // Dummy accounts for testing purposes
+    private String[][] dummyAccounts = {
+            {"00011", "password"},
+            {"00022", "password"}
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent Map = new Intent(this, MapsActivity.class);
-        startActivity(Map);
+        setContentView(R.layout.activity_main);
+
+        serialNumber = findViewById(R.id.serialNumber);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputSerial = serialNumber.getText().toString();
+                String inputPassword = password.getText().toString();
+
+                if (validateLogin(inputSerial, inputPassword)) {
+                    // Login successful, redirect to MapActivity
+                    Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                    intent.putExtra("serialNumber", inputSerial);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    // Login failed
+                    Toast.makeText(MainActivity.this, "Invalid Serial Number or Password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private boolean validateLogin(String serial, String password) {
+        for (String[] account : dummyAccounts) {
+            if (account[0].equals(serial) && account[1].equals(password)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- Examen-Opdracht/App/app/src/main/java/com/example/myapplication/MapsActivity.java
+++ Examen-Opdracht/App/app/src/main/java/com/example/myapplication/MapsActivity.java
@@ -4,7 +4,10 @@ import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Location;
@@ -38,6 +41,7 @@ import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Objects;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -45,10 +49,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private ActivityMapsBinding binding;
     private Circle lastClickedCircle;
     private CatList catlist;
+    private String serialNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        serialNumber = getIntent().getStringExtra("serialNumber");
 
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -67,6 +73,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         catlist = getCatList();
 
+        if (catlist.cats.isEmpty()) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Geen GPS Data")
+                    .setMessage("Er is geen gps data aanwezig, controleer of de GPS is ingeschakeld")
+                    .setPositiveButton("Uitloggen", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Intent intent = new Intent(MapsActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    }).setCancelable(false);
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+        }
+
         MarkerOptions userMarker = new MarkerOptions().position(UserPosition).title("U");
         mMap.addMarker(userMarker);
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(UserPosition, 17));
@@ -80,8 +102,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             CircleOptions circleOptions = new CircleOptions()
                     .center(data.position)
                     .radius(2.5) // Radius in meters
-                    .strokeColor(Color.DKGRAY)
-                    .fillColor(Color.DKGRAY)
+                    .strokeColor(Color.BLACK)
+                    .fillColor(Color.BLACK)
                     .clickable(true)
                     .zIndex(2); // Make the circle clickable
             Circle circle = mMap.addCircle(circleOptions);
@@ -102,35 +124,41 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     public CatList getCatList() {
-        CatList catList = new CatList("Nyx");
+        CatList catList;
+        if (Objects.equals(serialNumber, "00011")) {
+            catList = new CatList("Nyx");
 
-        catList.AddCatData(new CatData(
-                new LatLng(51.884573, 4.494143),
-                "10:01 18-06-2024"));
-        catList.AddCatData(new CatData(new LatLng(51.884248, 4.494434),
-                "10:03 18-06-2024"));
-        catList.AddCatData(new CatData(new LatLng(51.883873, 4.494298),
-                "10:08 18-06-2024"));
-        catList.AddCatData(new CatData(new LatLng(51.884171, 4.493095),
-                "10:13 18-06-2024"));
-        catList.AddCatData(new CatData(new LatLng(51.884091, 4.492711),
-                "10:15 18-06-2024"));
-        catList.AddCatData(new CatData(new LatLng(51.883517, 4.493090),
-                "10:17 18-06-2024"));
-        catList.AddCatData(new CatData(new LatLng(51.883320, 4.494497),
-                "10:26 18-06-2024"));
-        catList.AddCatData(new CatData(new LatLng(51.883684, 4.493495),
-                "10:30 18-06-2024"));
-        catList.AddCatData(new CatData(new LatLng(51.884377, 4.492529),
-                "10:37 18-06-2024"));
-        catList.AddCatData(new CatData(new LatLng(51.884408, 4.492870),
-                "10:42 18-06-2024"));
-        catList.AddCatData(new CatData(new LatLng(51.884861, 4.492893),
-                "10:45 18-06-2024"));
-        catList.AddCatData(new CatData(new LatLng(51.885485, 4.493907),
-                "10:48 18-06-2024"));
-        catList.AddCatData(new CatData(new LatLng(51.885624, 4.495075),
-                "10:56 18-06-2024"));
+            catList.AddCatData(new CatData(
+                    new LatLng(51.884573, 4.494143),
+                    "10:01 18-06-2024"));
+            catList.AddCatData(new CatData(new LatLng(51.884248, 4.494434),
+                    "10:03 18-06-2024"));
+            catList.AddCatData(new CatData(new LatLng(51.883873, 4.494298),
+                    "10:08 18-06-2024"));
+            catList.AddCatData(new CatData(new LatLng(51.884171, 4.493095),
+                    "10:13 18-06-2024"));
+            catList.AddCatData(new CatData(new LatLng(51.884091, 4.492711),
+                    "10:15 18-06-2024"));
+            catList.AddCatData(new CatData(new LatLng(51.883517, 4.493090),
+                    "10:17 18-06-2024"));
+            catList.AddCatData(new CatData(new LatLng(51.883320, 4.494497),
+                    "10:26 18-06-2024"));
+            catList.AddCatData(new CatData(new LatLng(51.883684, 4.493495),
+                    "10:30 18-06-2024"));
+            catList.AddCatData(new CatData(new LatLng(51.884377, 4.492529),
+                    "10:37 18-06-2024"));
+            catList.AddCatData(new CatData(new LatLng(51.884408, 4.492870),
+                    "10:42 18-06-2024"));
+            catList.AddCatData(new CatData(new LatLng(51.884861, 4.492893),
+                    "10:45 18-06-2024"));
+            catList.AddCatData(new CatData(new LatLng(51.885485, 4.493907),
+                    "10:48 18-06-2024"));
+            catList.AddCatData(new CatData(new LatLng(51.885624, 4.495075),
+                    "10:56 18-06-2024"));
+        }
+        else {
+            catList = new CatList("Noor");
+        }
         return catList;
     }
 
@@ -140,11 +168,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         if (lastClickedCircle != null) {
             lastClickedCircle.setFillColor(Color.BLACK); // Change this to your default color
             lastClickedCircle.setStrokeColor(Color.BLACK); // Change this to your default color
+            lastClickedCircle.setRadius(2.5); // Radius in meters
         }
 
         // Change the color of the current clicked circle to red
         circle.setFillColor(Color.RED);
         circle.setStrokeColor(Color.RED);
+        circle.setRadius(3.5);
 
         // Update the last clicked circle to the current one
         lastClickedCircle = circle;
@@ -163,5 +193,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         window.setText(String.format("%s: %s", catlist.name, data.dateTime));
         TextView distance = findViewById(R.id.kmWindow);
         distance.setText(String.format("%sKm", (float) Math.round(catlist.getDistance() * 10) / 10));
+    }
+
+    public void logoutButton(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Uitloggen?")
+                .setMessage("Wilt u uitloggen?")
+                .setPositiveButton("Uitloggen", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(MapsActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
